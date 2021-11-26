7dca20634ea82b05f4b56a323098fc50a3d358df
Erikisak <erik.isak@icloud.com>
2021-11-26 16:28:52 +0100

    javascript added and menu

---
+++ main.js
@@ -0,0 +1,22 @@
+const navMenu = document.getElementById('nav-menu'),
+ toggleMenu = document.getElementById('nav-toggle'),
+ closeMenu = document.getElementById('nav-close')
+
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+
